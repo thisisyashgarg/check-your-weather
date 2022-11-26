1d0eaa00099d7a50c00418d532aed5911de72161
Yash Garg <thisisyashgarg@gmail.com>
2022-11-26 19:22:27 +0530

    removed favicon bug

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const __dirname = path.resolve();
 import bodyParser from 'body-parser'
 import axios from 'axios'
 
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,27 +21,18 @@ const uri = "https://api.openweathermap.org/data/2.5/weather?q=" + req.body.city
 axios(uri)
 
 .then((response) => {
-    // const iconElement = response.data.weather[0].icon;
-    // const weatherDescription = response.data.weather[0].description;
-    // const cityName = response.data.name;
-    // const temperature = response.data.main.temp;
-    // const imageURL = 'http://openweathermap.org/img/wn/' + iconElement + '@2x.png';
-    // res.send(
-    //     `<h1>The weather condition is ${weatherDescription}<h1/>
-    //      <h2>The temperature of ${cityName} is ${temperature} degree celsius<h2/>
-    //      <img src = ${imageURL} >`
-    // );
-
     const iconElement = response.data.weather[0].icon;
     const weatherDescription = response.data.weather[0].description;
     const cityName = response.data.name;
     const temperature = response.data.main.temp;
-    // const imageURL = 'http://openweathermap.org/img/wn/' + iconElement + '@2x.png';
+    const imageURL = 'http://openweathermap.org/img/wn/' + iconElement + '@2x.png';
     res.send(
         `<h1>The weather condition is ${weatherDescription}<h1/>
          <h2>The temperature of ${cityName} is ${temperature} degree celsius<h2/>
-         `
+         <img src = ${imageURL} >`
     );
+
+    
 })
 .catch(() => res.send("<h1> 404 Error - Something went wrong - Please try again"));
     
